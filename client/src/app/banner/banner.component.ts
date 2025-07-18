f91f5ec1c665bf426fb49e8f847b614d62006832
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-07-18 15:32:39 +0200

    revert login method

--- client/src/app/banner/banner.component.ts
+++ client/src/app/banner/banner.component.ts
@@ -1,15 +1,8 @@
-import { Component, inject, effect } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import {
-  KEYCLOAK_EVENT_SIGNAL,
-  KeycloakEventType,
-  typeEventArgs,
-  ReadyArgs,
-  KeycloakService,
-} from 'keycloak-angular';
 
 @Component({
   selector: 'app-banner',
@@ -17,23 +10,4 @@ import {
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
-export class BannerComponent {
-  authenticated = false;
-
-  private keycloakService: KeycloakService;
-  private readonly keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
-
-  constructor() {
-    effect(() => {
-      const keycloakEvent = this.keycloakSignal();
-
-      if (keycloakEvent.type === KeycloakEventType.Ready) {
-        this.authenticated = typeEventArgs<ReadyArgs>(keycloakEvent.args);
-      }
-
-      if (keycloakEvent.type === KeycloakEventType.AuthLogout) {
-        this.authenticated = false;
-      }
-    });
-  }
-}
+export class BannerComponent {}
