0d71ff85ea6085513e099d2cfdc9af2228805bc8
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-07 13:09:04 +0200

    test: add MAT_DIALOG_DATA and MatDialogRef providers to SaveCdmDialogComponent tests

--- client/src/app/save-cdm-dialog/save-cdm-dialog.component.spec.ts
+++ client/src/app/save-cdm-dialog/save-cdm-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SaveCdmDialogComponent } from './save-cdm-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('SaveCdmDialogComponent', () => {
   let component: SaveCdmDialogComponent;
@@ -8,9 +9,19 @@ describe('SaveCdmDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SaveCdmDialogComponent]
-    })
-    .compileComponents();
+      imports: [SaveCdmDialogComponent],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            cdmName: '',
+            cdmVersion: '',
+            cdmOptions: {},
+          },
+        },
+        { provide: MatDialogRef, useValue: { close: jasmine.createSpy() } },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SaveCdmDialogComponent);
     component = fixture.componentInstance;
