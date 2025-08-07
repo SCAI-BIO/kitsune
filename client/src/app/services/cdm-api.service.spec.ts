13d6d5e3d5a50453cfd5a95374082c101350e753
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-07 09:32:36 +0200

    test: configure TestBed with HttpClient provider for CdmApiService tests

--- client/src/app/services/cdm-api.service.spec.ts
+++ client/src/app/services/cdm-api.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CdmApiService } from './cdm-api.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CdmApiService', () => {
   let service: CdmApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+    });
     service = TestBed.inject(CdmApiService);
   });
 
