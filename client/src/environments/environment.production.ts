ee2a21c3e51e2c780153c966c2d92d372adf7240
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2025-08-15 11:20:43 +0200

    chore: update production API URL

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   openApiUrl: 'https://api.kitsune.scai.fraunhofer.de',
-  cdmApiUrl: 'https://cdm-api.kitsune.scai.fraunhofer.de',
+  cdmApiUrl: 'https://api.cdm.kitsune.scai.fraunhofer.de',
   keycloak: {
     url: 'http://auth.k8s.bio.scai.fraunhofer.de/auth',
     realm: 'scai',
