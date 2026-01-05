a6b542e1c01a3327cb55a434ae6b48738635daf5
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-01-05 11:59:18 +0100

    test: migrate to vitest

--- client/src/app/extend-cdm-dialog/extend-cdm-dialog.component.spec.ts
+++ client/src/app/extend-cdm-dialog/extend-cdm-dialog.component.spec.ts
@@ -7,37 +7,37 @@ import { ExtendCdmDialogComponent } from './extend-cdm-dialog.component';
 import { OntologyApiService } from '../services/ontology-api.service';
 
 describe('ExtendCdmDialogComponent', () => {
-  let component: ExtendCdmDialogComponent;
-  let fixture: ComponentFixture<ExtendCdmDialogComponent>;
+    let component: ExtendCdmDialogComponent;
+    let fixture: ComponentFixture<ExtendCdmDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ExtendCdmDialogComponent],
-      providers: [
-        {
-          provide: MAT_DIALOG_DATA,
-          useValue: {
-            existingLabels: [],
-            studyColumnNames: [],
-          },
-        },
-        { provide: MatDialogRef, useValue: { close: jasmine.createSpy() } },
-        {
-          provide: OntologyApiService,
-          useValue: {
-            getOhdsiConceptById: () => of(null),
-            getOlsTermById: () => of(null),
-          },
-        },
-      ],
-    }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ExtendCdmDialogComponent],
+            providers: [
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {
+                        existingLabels: [],
+                        studyColumnNames: [],
+                    },
+                },
+                { provide: MatDialogRef, useValue: { close: vi.fn() } },
+                {
+                    provide: OntologyApiService,
+                    useValue: {
+                        getOhdsiConceptById: () => of(null),
+                        getOlsTermById: () => of(null),
+                    },
+                },
+            ],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ExtendCdmDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ExtendCdmDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
