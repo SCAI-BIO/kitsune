557c96ea7465110bdecd22f087e4f18b9cb54b23
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-03-27 16:35:58 +0100

    refactor: move ontology_id into path

--- api/app/routers/imports.py
+++ api/app/routers/imports.py
@@ -5,35 +5,22 @@ from starlette.background import BackgroundTasks
 
 from app.dependencies import get_current_user_payload
 from app.schemas import ObjectSchema
-from app.tasks import (
-    import_jsonl_task,
-    import_ols_terminology_task,
-    import_snomed_ct_task,
-)
+from app.tasks import import_jsonl_task, import_ols_ontology_task
 
 router = APIRouter(prefix="/imports", tags=["imports"])
 
 
-@router.put("/terminology", description="Import a terminology from OLS.")
+@router.post("/ols/{ontology_id}", description="Import a terminology from OLS.")
 def import_terminology(
     background_tasks: BackgroundTasks,
-    terminology_id: str,
+    ontology_id: str,
     user: Annotated[dict, Depends(get_current_user_payload)],
 ):
-    background_tasks.add_task(import_ols_terminology_task, terminology_id)
-    return {"message": f"{terminology_id} import started in the background"}
+    background_tasks.add_task(import_ols_ontology_task, ontology_id)
+    return {"message": f"{ontology_id} import started in the background"}
 
 
-@router.put("/terminology/snomed", description="Import whole SNOMED CT from OLS.")
-def import_snomed_ct(
-    background_tasks: BackgroundTasks,
-    user: Annotated[dict, Depends(get_current_user_payload)],
-):
-    background_tasks.add_task(import_snomed_ct_task)
-    return {"message": "SNOMED CT import started in the background"}
-
-
-@router.put("/jsonl", description="Import a JSONL file following the Weaviate schema")
+@router.post("/jsonl", description="Import a JSONL file following the Weaviate schema")
 def import_jsonl(
     background_tasks: BackgroundTasks,
     object_type: ObjectSchema,
