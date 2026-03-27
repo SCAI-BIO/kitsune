b392ce04b0aa24a031482fb496f0496c8588ba5e
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-03-27 16:35:18 +0100

    refactor: remove snomed ct import task

--- api/app/tasks/ols_tasks.py
+++ api/app/tasks/ols_tasks.py
@@ -3,13 +3,7 @@ from datastew.integrations.ols import OlsClient
 from app.database import PostgresClient
 
 
-def import_snomed_ct_task():
+def import_ols_ontology_task(ontology_id: str):
     with PostgresClient() as client:
-        task = OlsClient(client.vectorizer, "SNOMED CT", "snomed")
-        task.process_to_repository(client)
-
-
-def import_ols_terminology_task(terminology_id: str):
-    with PostgresClient() as client:
-        task = OlsClient(client.vectorizer, terminology_id, terminology_id)
+        task = OlsClient(client.vectorizer, ontology_id)
         task.process_to_repository(client)
