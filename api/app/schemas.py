f7448f433a6e18daabafd47bc436fc745ee4941a
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-03-27 13:01:17 +0100

    feat: add terminology schema

--- api/app/schemas.py
+++ api/app/schemas.py
@@ -1,7 +1,20 @@
 from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
 
 
 class ObjectSchema(Enum):
     TERMINOLOGY = "terminology"
     CONCEPT = "concept"
     MAPPING = "mapping"
+
+
+class TerminologyCreate(BaseModel):
+    name: str
+    short_name: str
+
+
+class TerminologyUpdate(BaseModel):
+    name: Optional[str] = None
+    short_name: Optional[str] = None
