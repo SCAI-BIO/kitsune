8d29eebad600d9f3d8dbd417e63adc38e035b45b
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-03-27 16:35:33 +0100

    refactor: remove snomed ct import task

--- api/app/tasks/__init__.py
+++ api/app/tasks/__init__.py
@@ -1,8 +1,4 @@
 from .file_tasks import import_jsonl_task
-from .ols_tasks import import_ols_terminology_task, import_snomed_ct_task
+from .ols_tasks import import_ols_ontology_task
 
-__all__ = [
-    "import_jsonl_task",
-    "import_ols_terminology_task",
-    "import_snomed_ct_task",
-]
+__all__ = ["import_jsonl_task", "import_ols_ontology_task"]
