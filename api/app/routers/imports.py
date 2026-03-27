c74050227fa18eefa8bdbd4741d9a214cc51ed2f
Mehmet Can Ay <mehmettt.can.ay@gmail.com>
2026-03-27 13:41:59 +0100

    refactor: secure endpoints

--- api/app/routers/imports.py
+++ api/app/routers/imports.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, HTTPException, UploadFile
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
 from starlette.background import BackgroundTasks
 
+from app.dependencies import get_current_user_payload
 from app.schemas import ObjectSchema
 from app.tasks.file_tasks import import_jsonl_task
 from app.tasks.ols_tasks import import_ols_terminology_task, import_snomed_ct_task
@@ -9,19 +12,31 @@ router = APIRouter(prefix="/imports", tags=["imports"])
 
 
 @router.put("/terminology", description="Import a terminology from OLS.")
-def import_terminology(background_tasks: BackgroundTasks, terminology_id: str):
+def import_terminology(
+    background_tasks: BackgroundTasks,
+    terminology_id: str,
+    user: Annotated[dict, Depends(get_current_user_payload)],
+):
     background_tasks.add_task(import_ols_terminology_task, terminology_id)
     return {"message": f"{terminology_id} import started in the background"}
 
 
 @router.put("/terminology/snomed", description="Import whole SNOMED CT from OLS.")
-def import_snomed_ct(background_tasks: BackgroundTasks):
+def import_snomed_ct(
+    background_tasks: BackgroundTasks,
+    user: Annotated[dict, Depends(get_current_user_payload)],
+):
     background_tasks.add_task(import_snomed_ct_task)
     return {"message": "SNOMED CT import started in the background"}
 
 
 @router.put("/jsonl", description="Import a JSONL file following the Weaviate schema")
-def import_jsonl(background_tasks: BackgroundTasks, object_type: ObjectSchema, file: UploadFile):
+def import_jsonl(
+    background_tasks: BackgroundTasks,
+    object_type: ObjectSchema,
+    file: UploadFile,
+    user: Annotated[dict, Depends(get_current_user_payload)],
+):
     if not file.filename or not file.filename.endswith(".jsonl"):
         raise HTTPException(status_code=400, detail="Invalid file type. Only JSONL files are accepted.")
 
